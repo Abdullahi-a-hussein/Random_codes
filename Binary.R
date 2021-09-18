6b98bb6c01b7fe068ccb20d6907c005885e0a256
Abdullahi Hussein <34224606+Abdullahi-a-hussein@users.noreply.github.com>
2021-09-18 12:46:32 -0400

    Add files via upload
    # Binary To Decimal and Back
    
    This file contains two symbol functions written in R. One converts **decimal** numbers to **binary** and the other does the reverse.

---
+++ Binary.R
@@ -0,0 +1,33 @@
+
+decimalToBinary = function(numb){
+  quotient = floor(numb/2); # Recording the quotient part of of the number
+ binary = c(numb%%2); # recording a vector of 1s and 0s
+  while (quotient != 0) {
+    reminder = quotient %% 2;
+    quotient = floor(quotient/2)
+    binary = append(binary, reminder, 0)
+    
+  };
+ binary;
+}
+
+decimalToBinary(2)
+
+
+ 
+binaryToDecimal = function(binaryvector){
+  n = length(binaryvector) - 1;
+  i = 1;
+  numb = binaryvector[i]*2^n;
+  
+  while (n != 0){
+    i = i + 1;
+    n = n - 1;
+    numb = numb +  2^n * binaryvector[i];
+  }
+  numb;
+}
+
+(x = decimalToBinary(3))
+(y = binaryToDecimal(x))
+
